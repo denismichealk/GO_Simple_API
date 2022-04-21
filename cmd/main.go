e4fd89b92f16b80941d5694c726a080da0a11013
Kamukama Denis <kampd80@gmail.com>
2022-04-21 21:50:09 +0300

    Pointed main func to handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,17 @@
 package cmd
 
 import (
+	"GO_Rest_API/pkg/handlers"
 	"github.com/gofiber/fiber"
 	_ "github.com/gofiber/fiber"
 )
 
 func main() {
 	app := fiber.New()
-	app.Get("/book/:id?", GetBook)
-	app.Get("/books", GetAllBooks)
-	app.Post("/book", AddBook)
-	app.Put("/book/:id", UpdateBook)
-	app.Delete("/book/:id", DeleteBook)
+	app.Get("/book/:id?", handlers.GetBook)
+	//app.Get("/books", handlers.GetAllBooks)
+	app.Post("/book", handlers.AddBook)
+	app.Put("/book/:id", handlers.UpdateBook)
+	app.Delete("/book/:id", handlers.DeleteBook)
 	app.Listen("8000")
 }
