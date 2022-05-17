670a639d2f4238bf28611779e453e6f487f151d6
Kamukama Denis <kampd80@gmail.com>
2022-05-17 08:23:19 +0300

    Removed unnecassry comments

--- Main.go
+++ Main.go
@@ -17,22 +17,6 @@ var database = "<your_database>"
 
 func main() {
 
-	// Build connection string
-	//connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
-	//	server, user, password, port, database)
-	//
-	//var err error
-	//
-	//// Create connection pool
-	//db, err = sql.Open("sqlserver", connString)
-	//if err != nil {
-	//	log.Fatal("Error creating connection pool: ", err.Error())
-	//}
-	//ctx := context.Background()
-	//err = db.PingContext(ctx)
-	//if err != nil {
-	//	log.Fatal(err.Error())
-	//}
 	fmt.Printf("Connected!\n")
 	app := fiber.New()
 	app.Get("/book", handlers.GetBook)
